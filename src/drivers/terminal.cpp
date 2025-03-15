569173cafbc306fb32239d389e346be94262834d
justanotherdev983 <106712@broklede.nl>
2025-03-15 22:22:17 +0100

    initial structure

---
+++ src/drivers/terminal.cpp
@@ -0,0 +1,20 @@
+#include "terminal.h"
+
+void terminal_init() {
+
+}
+void terminal_putchar(char c) {
+
+}
+void terminal_write(const char* data, size_t size) {
+
+}
+void terminal_writestring(const char* data) {
+
+}
+void terminal_setcolor(uint8_t color) {
+
+}
+void terminal_clear() {
+
+}
